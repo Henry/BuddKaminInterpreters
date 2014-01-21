2261516a76773e2ffb129bd9c4dc7e79f033c072
Henry <Henry>
2014-01-21 18:49:11 +0000

    Reformatting

--- Src/function.C
+++ Src/function.C
@@ -24,26 +24,29 @@ int Function::isClosure()
     return 0;
 }
 
-// default behavior for function applications is to evaluate
-// arguments
+// Default behavior for function applications is to evaluate arguments
 
 /// FunctionApply
 static ListNode* evalArgs(ListNode* args, Environment* rho)
 {
     if (args->isNil())
+    {
         return args;
+    }
+
     Expr newhead;
     Expression* first = args->head();
     first->eval(newhead, valueOps, rho);
-    return new ListNode(newhead(), evalArgs(args->tail(), rho));
-    newhead = 0;        // force garbage collection
+    ListNode* newargs = new ListNode(newhead(), evalArgs(args->tail(), rho));
+    newhead = 0;            // Force garbage collection
+
+    return newargs;
 }
 
 void Function::apply(Expr& target, ListNode* args, Environment* rho)
 {
     ListNode* newargs = evalArgs(args, rho);
     applyWithArgs(target, newargs, rho);
-    newargs = 0;        // force garbage collection
 }
 ///- FunctionApply
 
@@ -67,13 +70,21 @@ void UnaryFunction::apply(Expr& target, ListNode* args, Environment* rho)
     Function::apply(target, args, rho);
 }
 
-void UnaryFunction::applyWithArgs(Expr& target, ListNode* args,
-Environment* rho)
+void UnaryFunction::applyWithArgs
+(
+    Expr& target,
+    ListNode* args,
+    Environment* rho
+)
 {
     if (!fun)
+    {
         target = error("unaryfunction apply and no function");
+    }
     else
+    {
         fun(target, args->at(0));
+    }
 }
 
 //
@@ -91,13 +102,21 @@ void BinaryFunction::apply(Expr& target, ListNode* args, Environment* rho)
     Function::apply(target, args, rho);
 }
 
-void BinaryFunction::applyWithArgs(Expr& target, ListNode* args,
-Environment* rho)
+void BinaryFunction::applyWithArgs
+(
+    Expr& target,
+    ListNode* args,
+    Environment* rho
+)
 {
     if (!fun)
+    {
         target = error("binary function apply and no function");
+    }
     else
+    {
         fun(target, args->at(0), args->at(1));
+    }
 }
 
 //
@@ -105,8 +124,12 @@ Environment* rho)
 //
 
 /// IntegerBinaryFunctionApply
-void IntegerBinaryFunction::applyWithArgs(Expr& target, ListNode* args,
-Environment* rho)
+void IntegerBinaryFunction::applyWithArgs
+(
+    Expr& target,
+    ListNode* args,
+    Environment* rho
+)
 {
     Expression* left = args->at(0);
     Expression* right = args->at(1);
@@ -126,8 +149,12 @@ Environment* rho)
 //
 
 /// BooleanBinaryFunctionApply
-void BooleanBinaryFunction::applyWithArgs(Expr& target, ListNode* args,
-Environment* rho)
+void BooleanBinaryFunction::applyWithArgs
+(
+    Expr& target,
+    ListNode* args,
+    Environment* rho
+)
 {
     Expression* left = args->at(0);
     Expression* right = args->at(1);
@@ -138,9 +165,13 @@ Environment* rho)
     }
 
     if (fun(left->isInteger()->val(), right->isInteger()->val()))
+    {
         target = trueExpr();
+    }
     else
+    {
         target = falseExpr();
+    }
 }
 ///- BooleanBinaryFunctionApply
 
@@ -148,8 +179,7 @@ Environment* rho)
 //      user functions have argument names and body
 //
 
-UserFunction::UserFunction(ListNode* anames, Expression* bod,
-Environment* ctx)
+UserFunction::UserFunction(ListNode* anames, Expression* bod, Environment* ctx)
 {
     argNames = anames;
     body = bod;
@@ -169,8 +199,12 @@ int UserFunction::isClosure()
 }
 
 /// UserFunctionApply
-void UserFunction::applyWithArgs(Expr& target, ListNode* args,
-Environment* rho)
+void UserFunction::applyWithArgs
+(
+    Expr& target,
+    ListNode* args,
+    Environment* rho
+)
 {
     // number of args should match definition
     ListNode* an = argNames;
@@ -187,10 +221,14 @@ Environment* rho)
     // evaluate body in new environment
     Expression* bod = body();
     if (bod)
+    {
         bod->eval(target, valueOps, newrho);
+    }
     else
+    {
         target = 0;
+    }
 
-    newrho = 0; // force garbage collection
+    newrho = 0;             // Force garbage collection
 }
 ///- UserFunctionApply
